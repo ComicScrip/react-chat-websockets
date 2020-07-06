7d9bc8c4096b1d4bccd69b463bf633f5a3ab8192
Pierre Genthon <pierre.genthon@wildcodeschool.fr>
2020-07-06 09:35:25 +0200

    correction

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
+import socketIOClient from "socket.io-client";
 
 function App() {
+  const [messageList, setMessageList] = useState([])
+  const [nickName, setNickName] = useState('')
+  const [newMessageText, setNewMessageText] = useState('')
+  const [socket, setSocket] = useState(null)
+
+  useEffect(() => {
+    const socket = socketIOClient('http://localhost:3000')
+    setSocket(socket)
+    socket.on("initialMessageList", data => {
+      setMessageList(data)
+    });
+    socket.on("newMessageFromServer", data => {
+      console.log('received message from server', data)
+      setMessageList(messageList => [...messageList, data])
+    });
+    return () => socket.disconnect()
+  }, []);
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (newMessageText && nickName) {
+      socket.emit('newMessage', {author: nickName, text: newMessageText})
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Messages</h2>
+      {messageList.map(message => {
+        return (
+          <div key={message.id}>
+            {message.author} : {message.text}
+          </div>
+        )
+      })}
+
+      <form onSubmit={handleSubmit}>
+        <h2>New Message</h2>
+        <input type="text"
+               name="author"
+               placeholder="nickname"
+               value={nickName}
+               required
+               onChange={(e) => setNickName(e.target.value)}/>
+        <input type="text"
+               name="messageContent"
+               placeholder="message"
+               value={newMessageText}
+               required
+               onChange={(e) => setNewMessageText(e.target.value)}/>
+        <input type="submit" disabled={!nickName || !newMessageText} onClick={handleSubmit} value="send"/>
+      </form>
     </div>
   );
 }

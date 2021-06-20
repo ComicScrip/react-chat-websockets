51aac0c7900480515294bbe7dcc2ce544c048d3c
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-06-21 00:07:24 +0200

    upgrade deps

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const uniqid = require('uniqid');
 const app = express();
 const port = 3001;
 
-app.use(cors());
 const server = app.listen(port, () =>
   console.log(`app listening at http://localhost:${port}`)
 );

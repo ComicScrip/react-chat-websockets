96e5c1301d0874bcb81b57561949f5b2b121c63a
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-06-20 23:54:36 +0200

    upgrade deps

--- server/index.js
+++ server/index.js
@@ -2,28 +2,37 @@ const express = require('express');
 const cors = require('cors');
 const uniqid = require('uniqid');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(cors());
-const server = app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
-const io = require('socket.io').listen(server)
+const server = app.listen(port, () =>
+  console.log(`app listening at http://localhost:${port}`)
+);
+const socketIO = require('socket.io');
+
+const io = socketIO(server, {
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+});
 
 const messages = [
-  {id: uniqid(), author: 'server', text: 'welcome to WildChat'}
-]
+  { id: uniqid(), author: 'server', text: 'welcome to WildChat' },
+];
 
 io.on('connect', (socket) => {
   console.log('user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
 
   socket.emit('initialMessageList', messages);
 
   socket.on('messageFromClient', (messageTextAndAuthor) => {
-    const newMessage = {id: uniqid(), ...messageTextAndAuthor};
+    const newMessage = { id: uniqid(), ...messageTextAndAuthor };
     console.log('new message from a client: ', newMessage);
     messages.push(newMessage);
     io.emit('messageFromServer', newMessage);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,61 +1,71 @@
-import React, {useState, useEffect} from 'react'
-import socketIOClient from "socket.io-client";
+import React, { useEffect, useState } from 'react';
+import socketIOClient from 'socket.io-client';
 
 function App() {
-  const [messageList, setMessageList] = useState([])
-  const [nickName, setNickName] = useState('')
-  const [newMessageText, setNewMessageText] = useState('')
-  const [socket, setSocket] = useState(null)
+  const [messageList, setMessageList] = useState([]);
+  const [nickName, setNickName] = useState('');
+  const [newMessageText, setNewMessageText] = useState('');
+  const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const socket = socketIOClient('http://localhost:3000');
+    const socket = socketIOClient('http://localhost:3001');
     setSocket(socket);
 
     socket.on('initialMessageList', (messages) => {
       setMessageList(messages);
     });
 
-    socket.on('messageFromServer', newMessage => setMessageList(messageList => [...messageList, newMessage]));
-    return () => socket.disconnect()
-  }, [])
+    socket.on('messageFromServer', (newMessage) =>
+      setMessageList((messageList) => [...messageList, newMessage])
+    );
+    return () => socket.disconnect();
+  }, []);
 
-  const handleSubmit = e => {
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (newMessageText && nickName) {
-      socket.emit('messageFromClient', {text: newMessageText, author: nickName})
+      socket.emit('messageFromClient', {
+        text: newMessageText,
+        author: nickName,
+      });
     }
-  }
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <h2>Messages</h2>
-      {messageList.map(message => {
+      {messageList.map((message) => {
         return (
           <div key={message.id}>
             {message.author} : {message.text}
           </div>
-        )
+        );
       })}
 
       <form onSubmit={handleSubmit}>
         <h2>New Message</h2>
-        <input 
-          type="text"
-          name="author"
-          placeholder="nickname"
+        <input
+          type='text'
+          name='author'
+          placeholder='nickname'
           value={nickName}
           required
           onChange={(e) => setNickName(e.target.value)}
         />
-        <input 
-          type="text"
-          name="messageContent"
-          placeholder="message"
+        <input
+          type='text'
+          name='messageContent'
+          placeholder='message'
           value={newMessageText}
           required
           onChange={(e) => setNewMessageText(e.target.value)}
         />
-        <input type="submit" disabled={!nickName || !newMessageText} onClick={handleSubmit} value="send"/>
+        <input
+          type='submit'
+          disabled={!nickName || !newMessageText}
+          onClick={handleSubmit}
+          value='send'
+        />
       </form>
     </div>
   );
